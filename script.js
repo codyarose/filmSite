167ff3602bf8ae2c2c74dc8479ec7b2d3fd0ac12
Cody Rose <crose992@gmail.com>
2018-03-07 12:38:07 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var i;
+
+// if(i < num) {
+// 	i++;
+// 	if (i % 3 === 0 && i % 5 === 0) {
+// 		console.log("fizzbuzz");
+// 	} else if (i % 3 === 0) {
+// 		console.log("fizz");
+// 		} else if (i % 5 === 0) {
+// 		console.log("buzz");
+// 		} else {
+// 		console.log(i);
+// 		}
+// }
+function count() {
+	setInterval(fizzbuzz, 500);
+}
+
+function fizzbuzz() {
+	for(i = 1; i <= 15; i++) {
+		if(i % 3 === 0 && i % 5 ===0) {
+			console.log("fizzbuzz");
+		} else if (i % 3 === 0) {
+			console.log("fizz");
+		} else if (i % 5 === 0) {
+			console.log("buzz");
+		} else {
+			console.log(i);
+		}
+	}
+}
+
+fizzbuzz();
